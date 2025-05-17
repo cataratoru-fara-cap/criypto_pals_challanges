40444c43b0e1e310f7cd2f85d7079d14bc27b302
dragon <gabivaireanu@gmail.com>
2025-05-17 18:04:48 +0300

    finished c7

---
+++ Set 1/s7_decrypt_AES.py
@@ -0,0 +1,20 @@
+import base64
+from sys import argv
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+def decrypt_aes_128_ecb(key: bytes, file_path: str) -> bytes: 
+    cipher = Cipher(algorithm=algorithms.AES128(key=key), mode=modes.ECB())
+    f = open(file_path).read()
+    f = base64.b64decode(f)
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(f) + decryptor.finalize()
+    message = decrypted_data
+    return message
+
+if __name__ == "__main__":
+    key = b"YELLOW SUBMARINE"
+    message = decrypt_aes_128_ecb(key=key,file_path="/home/dragon/Sandbox/criypto_pals_challanges/Set 1/7_AESsd.txt")
+    print(message.decode())
+
+
+
